c4c1eaf836a77e02e4292d235bf6d2d9e2c0948b
Thorben Werner <werner@ismll.de>
2024-03-19 17:03:22 +0100

    initial commit

---
+++ datasets/__init__.py
@@ -0,0 +1,13 @@
+# Tabular Datasets
+from datasets.dna import DNA
+from datasets.splice import Splice
+from datasets.usps import USPS
+
+# Image Datasets
+from datasets.cifar10 import Cifar10
+from datasets.mnist import Mnist
+from datasets.fashion_mnist import FashionMnist
+
+# Text Datasets
+from datasets.topv2 import TopV2
+from datasets.news import News
